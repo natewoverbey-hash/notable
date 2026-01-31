a9aa2c5f45e128f109443109e5b307eb76082b34
natewoverbey-hash <natewoverbey@gmail.com>
2026-01-30 20:10:49 -0500

    Simplify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,7 @@ const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    const { userId } = await auth()
-    if (!userId) {
-      return Response.redirect(new URL('/sign-in', req.url))
-    }
+    await auth()
   }
 })
 
